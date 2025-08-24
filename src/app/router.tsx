39229dd74be7ff2714d2d056fd743e9ff554db5a
Joshua Balao <JoshuaCBalao@gmail.com>
2025-08-23 21:49:43 -0400

    Update router.tsx
    added new router for paths Product in Router file,

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "./screens/Home";
 import Cart from "./screens/Cart";
+import Product from "./screens/Product";
 
 const router = createBrowserRouter([
   { element: <App />,
     children: [
     {index: true, element: <Home />},
+    {path: "product/:id", element: <Product />},
     {path: "/cart", element: <Cart />},
-  ]}
+  ],
+  },
 ]);
 
 

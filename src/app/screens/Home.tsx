5f456e49eced80b1560fab827e0049abdf38fe71
Joshua Balao <JoshuaCBalao@gmail.com>
2025-08-23 22:49:07 -0400

    added images
    added each images for the store mock up and made sure that pathing works

--- src/app/screens/Home.tsx
+++ src/app/screens/Home.tsx
@@ -13,11 +13,11 @@ type Product = {
 const products: Product[]= [
     { id: '1', name: 'Dungeons & Dragons Player\'s Handbook', price: 49.99, image: '/images/dnd_players_handbook.jpg', tags: ['RPG', 'Core Rulebook'] },
     { id: '2', name: 'Pathfinder Core Rulebook 2nd Edition', price: 59.99, image: '/images/pathfinder_core_rulebook.jpg', tags: ['RPG', 'Core Rulebook'] },
-    { id: '3', name: 'Warhammer 40K Starter Set', price: 124.99, image: '/images/warhammer_40k_space_marine.jpg', tags: ['Miniature', 'Warhammer'] },
+    { id: '3', name: 'Warhammer 40K Starter Set', price: 124.99, image: '/img/MINIATURE/Warhammer-40k/WArhammer Starter Set.jpg', tags: ['Miniature', 'Warhammer'] },
     { id: '4', name: 'Magic: The Gathering Final Fantasy Booster Pack', price: 7.99, image: '/images/mtg_booster_pack.jpg', tags: ['Card Game', 'Magic'] },
     { id: '5', name: 'Yu-Gi-Oh! Blue-Eyes White Destiny Structure Deck', price: 9.99, image: '/images/yugioh_starter_deck.jpg', tags: ['Card Game', 'Yu-Gi-Oh'] },
     { id: '6', name: 'Board Game: Settlers of Catan', price: 49.99, image: '/images/settlers_of_catan.jpg', tags: ['Board Game'] },
-    { id: '7', name: 'Dice Set - Polyhedral RPG Dice', price: 14.99, image: '/images/polyhedral_dice_set.jpg', tags: ['Dice'] },
+    { id: '7', name: 'Dice Set Dusty Blue - Polyhedral RPG Dice', price: 14.99, image: '/images/polyhedral_dice_set.jpg', tags: ['Dice'] },
     { id: '8', name: 'Tabletop Terrain - Sci-Fi Scenery Set', price: 29.99, image: '/images/sci_fi_terrain_set.jpg', tags: ['Terrain'] },
     { id: '9', name: 'Dragonlance: Shadow of the Dragon Queen (EN)', price: 29.99, image: '/images/rpg_adventure_module.jpg', tags: ['Adventure Module'] },
     { id: '10', name: 'Arcane Tinmen Dragon Matte Emerald Sleeves- Dragon Shield', price: 4.99, image: '/images/dragon_shield_sleeves.jpg', tags: ['Accessories', 'Card Sleeves'] },

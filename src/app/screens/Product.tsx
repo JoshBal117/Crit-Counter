963a3f8d3bec093321e3f3d0a9c0467501915d3d
Joshua Balao <JoshuaCBalao@gmail.com>
2025-08-23 21:46:41 -0400

    Update Product page
    Edited further by adding in a UI and a display in grid column format for the products and making sure the products value is preset

--- src/app/screens/Product.tsx
+++ src/app/screens/Product.tsx
@@ -1,5 +1,5 @@
-import {useParams} from "react-router-dom";
-import { useLocation, usePArams, Link } from "react-router-dom";
+
+import { useLocation, useParams, Link } from "react-router-dom";
 
 type Product = {
     id: string;
@@ -46,6 +46,36 @@ function handleAddToCart() {
                 <div>
                     <h1 style={{ margin: 0 }}>{product.name}</h1>
                     <div style={{ marginTop: 8, fontWeight: 700}}>{money(product.price)}</div>
+
+                    {!!product.tags?.length && (
+                        <div style={{ marginTop: 8, display: "flex", gap: 8, flexWrap: "wrap"}}>
+                            {product.tags.map(t => (
+                                <span key={t} style={{ fontSize: 12, padding: "2px 8px",background: "#f1f5f9", borderRadius: 999}}>
+                                    {t.toUpperCase()}
+                                </span>
+                            ))}
+                    </div>
+                )}
+
+
+                <button
+                    onClick={handleAddToCart}
+                    style={{
+                        marginTop: 16,
+                        width: "100%",
+                        padding: "12px 16px",
+                        border: "none",
+                        borderRadius: 12,
+                        background: "#e11d48",
+                        color: "white",
+                        fontWeight: 608,
+                        cursor: "pointer",
+                    }}
+                    >
+                    Add to cart
+                    </button>
                 </div>
+            </div>
         </div>
-    )
+    );
+}

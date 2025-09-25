2f44753032ff5b1a7f80c9301645cd4d48b12386
Joshua Balao <JoshuaCBalao@gmail.com>
2025-09-24 22:44:17 -0400

    updated router and lock,json
    updated the router and the reinstalled the lock,json

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,19 +6,22 @@ import Product from "./screens/Product";
 import Checkout from "./screens/Checkout";
 import OrderConfirmation from "./screens/OrderConfirmation";
 
-const router = createBrowserRouter([
-  { element: <App />,
-    children: [
-    {index: true, element: <Home />},
-    {path: "product/:id", element: <Product />},
-    {path: "/cart", element: <Cart />},
-    {path: "checkout", element: <Checkout />},
-    {path: "order/:orderId/confirmation", element: <OrderConfirmation />},
- 
-
+const router = createBrowserRouter(
+  [
+    { 
+       element: <App />,
+      children: [
+        {index: true, element: <Home />},
+        {path: "product/:id", element: <Product />},
+        {path: "/cart", element: <Cart />},
+        {path: "checkout", element: <Checkout />},
+        {path: "order/:orderId/confirmation", element: <OrderConfirmation />},
+      ],
+    },
   ],
-  },
-]);
+  {basename: import.meta.env.BASE_URL
 
+  }
+);
 
 export default router;
